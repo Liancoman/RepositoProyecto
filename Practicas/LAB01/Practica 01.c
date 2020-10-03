0e50eac9df26694b7cf0e6a2bbac21b31e97f445
Esteban Chavez <estebanchvzal@gmail.com>
2020-10-02 19:42:43 -0500

    capsOn

--- Practicas/LAB01/Practica 01.c
+++ Practicas/LAB01/Practica 01.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <GENFUN.h>
+#include "stdio.h"
+#include "GENFUN.h"
 
-void main()
+void main(void)
 {
-	printf("hello");
+	uint8 au8array[11]= "estebanchvz";
+	GENFUN_vCapsOn (&au8array[0], 11);
+	printf("%s\n", au8array);
 }
 
-void GENFUN_vCapsOn (unit *pu8Src, unit8 u8SizeOfList)
+void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList)
 {
 	while ( u8SizeOfList != 0 )
 	{
-		if( *pu8Src > ASCII_LOW_THRESHOLD_OFF_OFF_ON && *pu8Src	< ASCII_HIGH_THRESHOLD_OFF_OFF_ON)
+		if( *pu8Src > ASCII_LOW_THRESHOLD_OFF_ON && *pu8Src < ASCII_HIGH_THRESHOLD_OFF_ON)
 		{
-			*pu8Src =ASCII_CONVERTION_FACTOR;
+			*pu8Src -= ASCII_CONVERTION_FACTOR;
 		}
 		else
 		{
