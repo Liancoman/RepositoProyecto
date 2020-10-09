7006e53483592cf5b7614b6bd57e7ae5a4ec1b28
Emiliano <emilianof.garcia@edu.uag.mx>
2020-10-08 20:30:21 -0500

    GetAverge y MemCopy

--- Practicas/LAB01/GENFUN.h
+++ Practicas/LAB01/GENFUN.h
@@ -8,22 +8,23 @@ typedef unsigned char u8Target;
 /*Definitions*/
 #define ASCII_HIGH_THRESHOLD_OFF_ON 123
 #define ASCII_LOW_THRESHOLD_OFF_ON 96
+#define ASCII_HIGH_THRESHOLD_OFF_ON_2 91
+#define ASCII_LOW_THRESHOLD_OFF_ON_2 64
 #define ASCII_CONVERTION_FACTOR 32
-#define FOR_START 0
-#define FOR_END 17
+#define END_SOFT 255
 
 /*Functions Prototypes*/ 
 
 void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList) ;
 void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList) ;
 
-uint32 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList) ;
-uint32 GENFUN_u8GetAverage (uint8 au8array[],uint8 *pu8Src, uint8 u8SizeOfList); 
+uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList) ;
+uint8 GENFUN_u8GetAverage (uint8 *pu8Src2, uint8 u8SizeOfList); 
 
 void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList) ;
 void GENFUN_u8MemCopy (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8SizeOfList) ;
 
 void GENFUN_vSortList (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8SizeOfList) ;
-void GENFUN_vSoftSignal (uint8 *pu8Src, uint8 *pu8Dest) ;
+void GENFUN_vSoftSignal (uint32 *pu8Src, uint32 *pu8Dest) ;
 
 void GENFUN_vFilterSignal (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8MaxVal, uint8 u8MinVal) ;
--- Practicas/LAB01/Practica 01.c
+++ Practicas/LAB01/Practica 01.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "GENFUN.h"
-#include "stdlib.h"
-
+#include <stdlib.h>
+#include <time.h> 
 
 void main(void)
 {
@@ -9,18 +9,24 @@ void main(void)
 	uint8 u8Char2Set;
 	uint8 saveocurrence;
 	uint8 u8Target;
+	uint8 au8arraynum[7]={2,4,2,4,8,7,4};
+	uint8 au8arraynum2[7]={0,0,0,0,0,0,0};
+	uint32 au32arraynum[255];
+	uint32 au32arraynum2[254];
+	uint32 saveoaverage;
+	uint8 i;
 
-	printf("Funcion CapsOn \n");
+	printf("\n		Funcion CapsOn \n");
 		printf("Before CapsOn: %s\n", au8array);
 		GENFUN_vCapsOn (&au8array[0], 18);
 		printf("After CapsOn: %s\n", au8array);
 	
-	printf("Funcion CapsOff \n");
+	printf("\n		Funcion CapsOff \n");
 		printf("Before CapsOff: %s\n", au8array);
 		GENFUN_vCapsOff (&au8array[0], 18);
 		printf("After CapsOff: %s\n", au8array);
 
-	printf("Funcion GetOccurence \n");
+	printf("\n		Funcion GetOccurence \n");
 		printf("¿Que letra desea busscar?\n");
 		scanf ("%c",&u8Target );
 		saveocurrence= GENFUN_u8GetOccurence(&au8array[0], u8Target, 18);
@@ -34,13 +40,25 @@ void main(void)
 		GENFUN_u8MemSet (&au8array[0], u8Target, 18);
 		printf("La nueva cadena es %s\n", au8array);
 
+	printf("\n		Funcion GetAverage \n");
+		printf("La funcion Average dara el promedio obtenido de la cadena siguiente:");
+		for(i=0;i<7;i++){printf(" %d ",au8arraynum[i]);	}
+		printf("\n");
+		saveoaverage=GENFUN_u8GetAverage(&au8arraynum[0],7);
+		printf("\nEl promedio obtenido es : %d",saveoaverage);
 
-	//printf("Funcion GetAverage \n");
-		//printf("Before GetAverage: %s\n", au8array);
-		//GENFUN_u8GetAverage(&au8array[0], 18);
-		//printf("After GetAverage: %s\n", au8array);
+	printf("\n		Funcion MemCopy \n");
+		printf("La funcion MemCopy copiara los valores de la cadena siguiente y estos seran asignados a una cadena diferente:");
+		for(i=0;i<7;i++){printf(" %d ",au8arraynum[i]);	}
+		printf("\nLa cadena vacia es la siguinete: ");
+		for(i=0;i<7;i++){printf(" %d ",au8arraynum2[i]);}
+		printf("\n");
+		GENFUN_u8MemCopy(&au8arraynum[0],&au8arraynum2[0],7);
+		printf("\n Ya se se realizo la funcion MemCopy y el resultado es el siguiente : ");
+		for(i=0;i<7;i++){printf(" %d ",au8arraynum2[i]);}
 
-
+	printf("\n		Funcion SoftSignal \n");
+	GENFUN_vSoftSignal (&au32arraynum[0], &au32arraynum2[0]);		
 }
 
 void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList)
@@ -98,24 +116,18 @@ uint32 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList)
 	return Contrador;
 }
 
-uint32 GENFUN_u8GetAverage (uint8 au8array[],uint8 *pu8Src, uint8 u8SizeOfList)
+uint8 GENFUN_u8GetAverage (uint8 *pu8Src, uint8 u8SizeOfList)
 {
- 	uint32 Variable;
-	uint32 Contrador;
-    printf("\nLa función hará el promedio de letras en el au8array\n");
-    for (Variable=FOR_START ; Variable<FOR_END ; Variable++) 
-    {    
-            if ( au8array[Variable] == *pu8Src )
-            {
-                Contrador++;
-            }
-            else
-			{
-				/*Nothing to do*/
-			}   
-    	printf("\nLa letra %c se repite: %d veces.",au8array[Variable], Contrador);        
-    }
-	return 0;
+	uint32 Sumadevalores=0;
+	uint8 div=u8SizeOfList;
+	while ( u8SizeOfList != 0 )
+	{
+		Sumadevalores+= *pu8Src;
+		pu8Src++;
+		u8SizeOfList--;
+	}
+	Sumadevalores/=div; 
+	return Sumadevalores;
 }
 
 void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList) 
@@ -130,10 +142,13 @@ void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList)
 
 void GENFUN_u8MemCopy (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8SizeOfList) 
 {
-
-/* 
-La función deberá recibir la dirección de dos arreglos, y copiar la información 
-de uno al otro.*/ 
+	while ( u8SizeOfList != 0 )
+	{
+		*pu8Dest= *pu8Src;
+		pu8Src++;
+		pu8Dest++;
+		u8SizeOfList--;
+	}
 }
 
 void GENFUN_vSortList (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8SizeOfList) 
@@ -155,15 +170,18 @@ La función deberá recibir una cadena con N elementos desordenados y reescribir
 la misma lista ordenada en la dirección indicada en orden ascendente. */
 }
 
-void GENFUN_vSoftSignal (uint8 *pu8Src, uint8 *pu8Dest) 
+void GENFUN_vSoftSignal (uint32 *pu32Src, uint32 *pu32Dest) 
 {
-/*  Parámetros: 
--pu8Src: Apuntador que recibe la dirección de la cadena fuente. 
--pu8Dest: Apuntador que recibe la dirección de la cadena destino. 
-Retorno: N/A 
+	uint8 indice = END_SOFT;
+	while(indice!= 0)
+	{
+		*pu32Src = rand() % 10;
+		indice--;
+	}
 
-La función deberá recibir una cadena con 255 elementos (simulando un grafica
-numérica) la cual se tendrá que suavizar aplicando un promedio entre los 2 
+	
+/* 
+La función recibe una cadena con 255 elementos la cual se tendrá que suavizar aplicando un promedio entre los 2 
 elementos más cercanos de toda la cadena. 
 Ejemplo (Primer Elemento Destino = (Primer Elemento Fuente + Segundo Elemento
 Fuente) / 2) */
