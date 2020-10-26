eb133b8e009714bc520e699a0cd5b355b5259f2c
Emiliano <emilianof.garcia@edu.uag.mx>
2020-10-26 02:36:50 -0600

    Not yet

--- Practicas/LABXX/Mines.c
+++ Practicas/LABXX/Mines.c
@@ -7,9 +7,9 @@
 
 int main()
 {
-  int buscaminas[RENGLON][COLUMNA] = {0}, i, j;
-  int nivel, bombas;                                    
-  int gameStatus=0, victoria=0;            
+  int buscaminas[RENGLON][COLUMNA] = {0};
+  int i,j,nivel, bombas;                                    
+  int gameStatus=0, victoria=0,gameCounter=0;            
   srand(time(NULL)); 
 
   //Hacer bucle
@@ -24,31 +24,36 @@ int main()
         bombas = 10;
       break;
       case 2:
-        bombas = 15;
+        bombas = 20;
       break;
       case 3:
-        bombas = 20;
+        bombas = 30;
       break;
       default: 
         printf("Seleccion invalida, intente de nuevo.\n\n");
       break;                 
     }
 
-  colocar_bombas(buscaminas[][], bombas);
+  colocar_bombas(&buscaminas[0][0], bombas);
 
-  //Esto se repite hasta que se acabe el juego 
+ //Esto se repite hasta que se acabe el juego 
   while(gameStatus != -1 || victoria == 1)
-  {      
+  { 
+    //system(cls); 
+    //Empieza el juego     
     printf("\n-Buscaminas-\n\n");
-    imprimir_tablero(buscaminas[][], gameStatus);
+    //Imprimimos tablero 1ra vez puro #
+    imprimir_tablero(&buscaminas[0][0], gameStatus);  //ojo
+    
     printf("\n"); 
     printf("*Ingrese la tirada[i, j]: ");
-    scanf("%d%d",&i, &j);
+    scanf("%d",&i);
+    scanf("%d",&j);
     
     if(buscaminas[i][j] == 9)
     {                              
       gameStatus = -1;
-      imprimir_tablero(buscaminas[][], gameStatus);
+      imprimir_tablero(&buscaminas[0][0], gameStatus);
       printf("\t ¡¡¡Perdiste, el juego ha terminado!!");
       getchar();              
     }
@@ -56,39 +61,55 @@ int main()
     {
       buscaminas[i][j] = 1;
     }
+
     if(gameCounter == (255 - bombas) )
     {
       victoria = 1;
-      imprimir_tablero(buscaminas[][], gameStatus);
+      imprimir_tablero(&buscaminas[0][0], gameStatus);
       printf("\t  ¡¡¡Has ganado, felicidades!!!");
       getchar();
     }
+    gameCounter++;
   }
   for(i = 0; i <= RENGLON - 1; i++)
   for(j = 0; j <= COLUMNA - 1; j++)
-    buscaminas[i][j] = 0;   
+    buscaminas[i][j] = 1;   
 }     
     
 
-void colocar_bombas(int minas[][], int bum)
+void colocar_bombas(int *minas, int bum)  //Revisar las  @
 {
-  int i, renglon, columna;   
+  int i,j, renglon, columna;
+  int arreglo [15][15];
+
   for(i = 0; i < bum; i++)
   {
-    renglon = 0 + rand() % 15;
+    renglon = 0 + rand() % 14;
     columna = 0 + rand() % 15;
-    minas[renglon][columna] = 9;
+    printf("%d %d\n",renglon,columna );
+    for(j=0;j<(renglon*15)+columna;j++)
+    {
+      minas++;
+    }
+    *minas=9;
+    for(j=0;j<=(renglon*15)+columna;j++)
+    {
+      minas--;
+    }
+
   }
 } 
 
-void imprimir_tablero(int tablero[][], int perder)
+void imprimir_tablero(int *tablero, int perder)
 {
-    int i, j;
+    int i, j,save2;
+    int arreglo2[15][15];
     //Se imprime los numeros de arriba
-    for(i = 0; i <= RENGLON -1; i++)  printf("%d ", i);
+    for(i = 0; i <= RENGLON -6; i++)  printf("%d  ", i);
+      for(i = 10; i <=14; i++)  printf("%d ", i);
     printf("\n");
     //Se imprime la separación de numeros con el juego
-    for(i = 0; i <= COLUMNA - 1; i++)
+    for(i = 0; i <= COLUMNA + 7; i++)
     printf("--");
     printf("\n");
     
@@ -97,14 +118,72 @@ void imprimir_tablero(int tablero[][], int perder)
     {
       for(j = 0; j <= COLUMNA - 1; j++)
       {
-        if(tablero[i][j] == 1)  printf("%d ", tablero[i][j]);
+        arreglo2[i][j]=*tablero;
+        if(arreglo2[i][j] == 1)
+        {
+          //revisar_alrededor(&arreglo2[0][0],i,j);
+          printf("f  ");
+          
+        }  
         
-        else if((tablero[i][j] == 9) && (perder == -1))   printf("%c ", '@');
-        
-        else    printf("%c ", '#');        
+        else if((arreglo2[i][j] == 9) && (perder == -1))
+        {   
+          printf("%c  ", '@');
+          tablero++;
+        }
+        else    printf("%c  ", '#');   
+
+
+        tablero++;     
       }
       printf("| %d", i);   
       printf("\n"); 
     }
     
 }
+
+/*void revisar_alrededor(int *tablero2, int a, int b)
+{
+  int arreglo3[15][15];
+  int i,j,sum=0;
+  
+  llegar al cerca 1
+  for(j=0;j<=((b-1)*15)+(a-1)  ;j++ )
+  {
+    tablero2++;
+  }
+  
+  ver los 3 valores de arriba si son 9
+  for(j=0;j<=2 ;j++ )
+  {
+    if(*tablero2 == 9) sum++
+    tablero2++;
+  }
+  Se elimino algo importante
+  llegar al cerca 1
+  for(j=0;j<=((b-1)*15)+(a-1)  ;j++ )
+  {
+    tablero2++;
+  }
+  
+  ver los 3 valores de arriba si son 9
+  for(j=0;j<=2 ;j++ )
+  {
+    if(*tablero2 == 9) sum++
+    tablero2++;
+  }
+
+  llegar al cerca 1
+  for(j=0;j<=((b-1)*15)+(a-1)  ;j++ )
+  {
+    tablero2++;
+  }
+  
+  ver los 3 valores de arriba si son 9
+  for(j=0;j<=2 ;j++ )
+  {
+    if(*tablero2 == 9) sum++
+    tablero2++;
+  } 
+  printf("%d ",sum);
+}*/
--- Practicas/LABXX/Mines.h
+++ Practicas/LABXX/Mines.h
@@ -1,6 +1,6 @@
 #define RENGLON 15
 #define COLUMNA 15
 
-void imprimir_tablero(int tablero[][], int perder);
-
-void colocar_bombas(int minas[][], int bum);
+void imprimir_tablero(int tablero[], int perder);
+void colocar_bombas(int minas[], int bum);
+void revisar_alrededor(int tablero[], int a, int b);
