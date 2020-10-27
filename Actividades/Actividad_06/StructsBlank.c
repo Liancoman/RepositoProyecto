b7669cde2851b788d100ea2e481b5217cb425b4c
Esteban Chavez <estebanchvzal@gmail.com>
2020-10-27 01:24:18 -0600

    Actividad_06_Terminada

--- Actividades/Actividad_06/StructsBlank.c
+++ Actividades/Actividad_06/StructsBlank.c
@@ -29,7 +29,7 @@ tstParkingInfo astMyParks[enMaxSectors] = INIT_PARKING_SLOTS;						/*Struct to U
 
 void main ( void )
 {
-	uint8 u8InputAction = 0;
+	uint8 u8InputAction=0;
 	tenParkingSectors enSector; 
 	uint8 u8ConfirmationFromAction = 0;
 	while(1)
@@ -52,27 +52,27 @@ void main ( void )
 		{
 			if( u8InputAction == 1 )
 			{
-
+				ShowAvailableSlots (enSector);
 			}
 			else if( u8InputAction == 2 )
 			{
-
+				ShowNotAvailableSlots (enSector);
 			}
 			else if( u8InputAction == 3 )
 			{
-
+				ShowEarnedMoney (enSector);
 			}
 			else if( u8InputAction == 4 )
 			{
-
+				u8AddCarToSector (enSector);
 			}
 			else if ( u8InputAction == 5 )
 			{
-
+				u8RemoveCarFromSector (enSector);
 			}
 			else
 			{
-
+				u8ImportPaymentOfSlot (enSector);
 			}
 			printf("\n\n");
 		}
@@ -80,39 +80,302 @@ void main ( void )
 		{
 			/*Not a valid sector*/
 		}
-
 	}
 }
 
 /*F1*/
 void ShowAvailableSlots ( tenParkingSectors enCurrentSector )
-{
-
+{	
+	
+	if(enCurrentSector == 0)
+	{
+		if(astMyParks[enCurrentSector].u8AvailableSlots !=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8AvailableSlots);
+		}
+		else
+		{
+			printf("No Available Slots");
+		}
+	}
+	if(enCurrentSector == 1)
+	{
+		if(astMyParks[enCurrentSector].u8AvailableSlots !=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8AvailableSlots);
+		}
+		else
+		{
+			printf("No Available Slots");
+		}
+	}
+	if(enCurrentSector == 2)
+	{
+		if(astMyParks[enCurrentSector].u8AvailableSlots !=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8AvailableSlots);
+		}
+		else
+		{
+			printf("No Available Slots");
+		}
+	}
+	if(enCurrentSector == 3)
+	{
+		if(astMyParks[enCurrentSector].u8AvailableSlots !=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8AvailableSlots);
+		}
+		else
+		{
+			printf("No Available Slots");
+		}
+	}
+	if(enCurrentSector == 4)
+	{
+		if(astMyParks[enCurrentSector].u8AvailableSlots !=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8AvailableSlots);
+		}
+		else
+		{
+			printf("No Available Slots");
+		}
+	}
 }
 
 /*F2*/
 void ShowNotAvailableSlots ( tenParkingSectors enCurrentSector )
 {
-
+	if(enCurrentSector == 0)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots >=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8NotAvailableSlots);
+		}
+		else
+		{
+			printf("All of the Slots are free to use");
+		}
+	}
+	if(enCurrentSector == 1)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots >=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8NotAvailableSlots);
+		}
+		else
+		{
+			printf("All of the Slots free to use");
+		}
+	}
+	if(enCurrentSector == 2)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots >=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8NotAvailableSlots);
+		}
+		else
+		{
+			printf("All of the Slots free to use");
+		}
+	}
+	if(enCurrentSector == 3)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots >=0)
+		{
+			printf("In the current sector are %d available Slots", astMyParks[enCurrentSector].u8NotAvailableSlots);
+		}
+		else
+		{
+			printf("All of the Slots free to use");
+		}
+	}
+	if(enCurrentSector == 4)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots >=0)
+		{
+			printf("In the current sector are %d Not available Slots", astMyParks[enCurrentSector].u8NotAvailableSlots);
+		}
+		else
+		{
+			printf("All of the Slots free to use");
+		}
+	}
 }
 
 /*F3*/
 void ShowEarnedMoney ( tenParkingSectors enCurrentSector )
 {
-
+	uint8 EARN=0;
+	if(enCurrentSector == 0)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			EARN = astMyParks[enCurrentSector].u8NotAvailableSlots * PRICE;
+			printf("The courrent sector has eaarn %d dolars\n", EARN);
+		}
+	}
+	if(enCurrentSector == 1)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			EARN = astMyParks[enCurrentSector].u8NotAvailableSlots * PRICE;
+			printf("The courrent sector has eaarn %s dolars\n", EARN);
+		}
+	}
+	if(enCurrentSector == 2)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			EARN = astMyParks[enCurrentSector].u8NotAvailableSlots * PRICE;
+			printf("The courrent sector has eaarn %d dolars\n", EARN);
+		}
+	}
+	if(enCurrentSector == 3)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			EARN = astMyParks[enCurrentSector].u8NotAvailableSlots * PRICE;
+			printf("The courrent sector has eaarn %d dolars\n", EARN);
+		}
+	}
+	if(enCurrentSector == 4)
+	{
+		if(astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			EARN = astMyParks[enCurrentSector].u8NotAvailableSlots * PRICE;
+			printf("The courrent sector has eaarn %d dolars\n", EARN);
+		}
+	}
 }
 /*F4*/
 uint8 u8AddCarToSector( tenParkingSectors enCurrentSector )
 {
-
+	if(enCurrentSector == 0)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots --;
+		astMyParks[enCurrentSector].u8NotAvailableSlots ++;
+		printf("Your car has enter to the sector");
+	}
+	if(enCurrentSector == 1)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots --;
+		astMyParks[enCurrentSector].u8NotAvailableSlots ++;
+		printf("Your car has enter to the sector");
+	}
+	if(enCurrentSector == 2)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots --;
+		astMyParks[enCurrentSector].u8NotAvailableSlots ++;
+		printf("Your car has enter to the sector");
+	}
+	if(enCurrentSector == 3)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots --;
+		astMyParks[enCurrentSector].u8NotAvailableSlots ++;
+		printf("Your car has enter to the sector");
+	}
+	if(enCurrentSector == 4)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots --;
+		astMyParks[enCurrentSector].u8NotAvailableSlots ++;
+		printf("Your car has enter to the sector");
+	}
+	return 0;
 }
+
 /*F5*/
 uint8 u8RemoveCarFromSector( tenParkingSectors enCurrentSector )
 {
-
+	if(enCurrentSector == 0)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots ++;
+		astMyParks[enCurrentSector].u8NotAvailableSlots --;
+		printf("Your car has been removed from the sector");
+	}
+	if(enCurrentSector == 1)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots ++;
+		astMyParks[enCurrentSector].u8NotAvailableSlots --;
+		printf("Your car has been removed from the sector");
+	}
+	if(enCurrentSector == 2)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots ++;
+		astMyParks[enCurrentSector].u8NotAvailableSlots --;
+		printf("Your car has been removed from the sector");
+	}
+	if(enCurrentSector == 3)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots ++;
+		astMyParks[enCurrentSector].u8NotAvailableSlots --;
+		printf("Your car has been removed from the sector");
+	}
+	if(enCurrentSector == 4)
+	{
+		astMyParks[enCurrentSector].u8AvailableSlots ++;
+		astMyParks[enCurrentSector].u8NotAvailableSlots --;
+		printf("Your car has been removed from the sector");
+	}
+	return 0;
 }
 /*F6*/
 void u8ImportPaymentOfSlot( tenParkingSectors enCurrentSector )
 {
-
+	if(enCurrentSector == 0)
+	{
+		if (astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			printf("Your car will be safe with us\n");
+		}
+		else
+		{
+			printf("Your car isnt in this sector\n");
+		}
+	}
+	if(enCurrentSector == 1)
+	{
+		if (astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			printf("Your car will be safe with us\n");
+		}
+		else
+		{
+			printf("Your car isnt in this sector\n");
+		}
+	}
+	if(enCurrentSector == 2)
+	{
+		if (astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			printf("Your car will be safe with us\n");
+		}
+		else
+		{
+			printf("Your car isnt in this sector\n");
+		}
+	}
+	if(enCurrentSector == 3)
+	{
+		if (astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			printf("Your car will be safe with us\n");
+		}
+		else
+		{
+			printf("Your car isnt in this sector\n");
+		}
+	}
+	if(enCurrentSector == 4)
+	{
+		if (astMyParks[enCurrentSector].u8NotAvailableSlots !=0)
+		{
+			printf("Your car will be safe with us\n");
+		}
+		else
+		{
+			printf("Your car isnt in this sector\n");
+		}
+	}
 }
