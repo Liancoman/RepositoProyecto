e89129a6280a6f45bc697dea223e0cab916d07a4
Daniela Silva <madanisi@outlook.com>
2020-11-06 22:43:31 -0600

    Notación hungara

--- Practicas/LABXX/Mines.c
+++ Practicas/LABXX/Mines.c
@@ -7,30 +7,30 @@
 
 void main(void)
 {
-  int buscaminas[RENGLON][COLUMNA]={0};
-  int posicioni,posicionj,nivel,bombas,i,j;                                    
-  int gameStatus=0, victoria=0,gameCounter=0;            
+  uint8 au8buscaminas[RENGLON][COLUMNA]={0};
+  uint8 u8posicioni, u8posicionj,u8nivel,u8bombas,i,j;                                    
+  uint8 u8gameStatus=0, u8ictoria=0, u8gameCounter=0;            
   srand(time(NULL)); 
 
   //Hacer bucle
     printf("\n   >>>>Buscaminas<<<<\n\n");
     printf("Niveles:\n1)Facil\n2)Normal\n3)Dificil\n");
     printf("Ingrese el nivel que desea :");
-    scanf("%d", &nivel);
+    scanf("%d", &u8nivel);
       
     switch(nivel)
     {
       case 1:
-        bombas = 10;
-        colocar_bombas(buscaminas, bombas);
+        u8bombas = 10;
+        colocar_bombas(au8buscaminas, u8bombas);
       break;
       case 2:
-        bombas = 20;
-        colocar_bombas(buscaminas, bombas);
+        u8bombas = 20;
+        colocar_bombas(au8buscaminas, u8bombas);
       break;
       case 3:
-        bombas = 30;
-        colocar_bombas(buscaminas, bombas);
+        u8bombas = 30;
+        colocar_bombas(au8buscaminas, u8bombas);
       break;
       default: 
         printf("Seleccion invalida, intente de nuevo.\n\n");
@@ -38,62 +38,62 @@ void main(void)
     }
 
  //Esto se repite hasta que se acabe el juego 
-  while(gameStatus != -1 || victoria == 1)
+  while(au8gameStatus != -1 || au8victoria == 1)
   { 
     system("cls"); 
     //Empieza el juego     
     printf("\n-Buscaminas-\n\n");
     //Imprimimos tablero 1ra vez puro #
-    imprimir_tablero(buscaminas, gameStatus);  //ojo
+    imprimir_tablero(au8buscaminas, u8gameStatus);  //ojo
     
     printf("\n"); 
     printf("*Ingrese la tirada[i, j]: ");
-    scanf("%d",&posicionj);
-    scanf("%d",&posicioni);
+    scanf("%d",&u8posicionj);
+    scanf("%d",&u8posicioni);
     
     
-    if(buscaminas[posicioni][posicionj] == 3)
+    if(au8buscaminas[u8posicioni][u8posicionj] == 3)
     {                              
       gameStatus = -1;
-      imprimir_tablero(buscaminas, gameStatus);
+      imprimir_tablero(au8buscaminas, u8gameStatus);
       printf("\t ¡¡¡Perdiste, el juego ha terminado!!");
                   
     }
     else
     {
-      buscaminas[posicioni][posicionj] = 1;
+      au8buscaminas[u8posicioni][u8posicionj] = 1;
     }
 
-    if( gameCounter == (255 - bombas) )
+    if( u8gameCounter == (255 - u8bombas) )
     {
-      victoria = 1;
-      imprimir_tablero(buscaminas, gameStatus);
+      uint8 u8victoria = 1;
+      imprimir_tablero(au8buscaminas, u8gameStatus);
       printf("  ¡¡¡Has ganado, felicidades!!!");
       
     }
-    gameCounter++;
+    u8gameCounter++;
   }
   for(i = 0; i <= RENGLON - 1; i++)
     for(j = 0; j <= COLUMNA - 1; j++)
-      buscaminas[i][j] = 0;   
+      au8buscaminas[i][j] = 0;   
 }     
     
-void colocar_bombas(int buscaminas[RENGLON][COLUMNA], int bum)
+void colocar_bombas(uint8 au8buscaminas[RENGLON][COLUMNA], uint8 u8bum)
 {
-     int i, renglon, columna;
+     uint8 i, u8renglon, u8columna;
     
      for(i = 1; i <= bum; i++)
      {
-           renglon = 0 + rand() % 14;
-           columna = 0 + rand() % 14;
+           u8renglon = 0 + rand() % 14;
+           u8columna = 0 + rand() % 14;
           
-           buscaminas[renglon][columna]= 3 ;
+           au8buscaminas[u8renglon][u8columna]= 3 ;
      }
 }
 
-void imprimir_tablero(int tablero[RENGLON][COLUMNA], int perder)
+void imprimir_tablero(uint8 au8tablero[RENGLON][COLUMNA], uint8 u8perder)
 {
-    int i, j;
+    uint8 i, j;
     //Se imprime los numeros de arriba
     for(i = 0; i <= RENGLON -6; i++)  printf("%d  ", i);
     for(i = 10; i <=14; i++)  printf("%d ", i);
@@ -106,11 +106,11 @@ void imprimir_tablero(int tablero[RENGLON][COLUMNA], int perder)
     {
       for(j = 0; j <= COLUMNA - 1; j++)
       {
-        if(tablero[i][j] == 1)
+        if(au8tablero[i][j] == 1)
         {
-          revisar_alrededor (tablero,i,j);
+          revisar_alrededor (au8tablero,i,j);
         }
-        else if((tablero[i][j] ==3) && (perder == -1))
+        else if((au8tablero[i][j] ==3) && (u8perder == -1))
           printf("%c  ", '@');
         else
           printf("%c  ", '#');        
@@ -120,54 +120,54 @@ void imprimir_tablero(int tablero[RENGLON][COLUMNA], int perder)
     }
 }
 
-void revisar_alrededor(int Tablero[RENGLON][COLUMNA],int Fila, int Columna)
+void revisar_alrededor(uint8 au8Tablero[RENGLON][COLUMNA],uint8 u8Fila, uint8 u8Columna)
 {
-  int Conteo = 0, FilaInicio, FilaFin, ColumnaInicio, ColumnaFin;
-  if (Fila <= 0)
+  uint8 u8Conteo = 0, u8FilaInicio, u8FilaFin, u8ColumnaInicio, u8ColumnaFin;
+  if (u8Fila <= 0)
   {
-  FilaInicio = 0;
+  u8FilaInicio = 0;
   }
   else 
   {
-  FilaInicio = Fila - 1;
+  u8FilaInicio = u8Fila - 1;
   }
   if 
-  (Fila + 1 >= RENGLON)
+  (u8Fila + 1 >= RENGLON)
   {
-    FilaFin = RENGLON - 1;
+    u8FilaFin = RENGLON - 1;
   } 
   else 
   {
-    FilaFin = Fila + 1;
+    u8FilaFin = u8Fila + 1;
   }
 
-  if (Columna <= 0) 
+  if (u8Columna <= 0) 
   {
-    ColumnaInicio = 0;
+    u8ColumnaInicio = 0;
   } 
   else 
   {
-    ColumnaInicio = Columna - 1;
+    u8ColumnaInicio = u8Columna - 1;
   }
-  if (Columna + 1 >= COLUMNA) 
+  if (u8Columna + 1 >= COLUMNA) 
   {
-    ColumnaFin = COLUMNA - 1;
+    u8ColumnaFin = COLUMNA - 1;
   } 
   else 
   {
-    ColumnaFin = Columna + 1;
+    u8ColumnaFin = u8Columna + 1;
   }
-  int m;
-  for (m = FilaInicio; m <= FilaFin; m++)
+  uint8 m;
+  for (m = u8FilaInicio; m <= u8FilaFin; m++)
   {
-    int l;
-    for (l = ColumnaInicio; l <= ColumnaFin; l++)
+    uint8 l;
+    for (l = u8ColumnaInicio; l <= u8ColumnaFin; l++)
     {
-      if (Tablero[m][l] == 3) 
+      if (au8Tablero[m][l] == 3) 
       {
-        Conteo++;
+        u8Conteo++;
       }
     }
   }
-  printf("%d  ",Conteo);
+  printf("%d  ",u8Conteo);
 }
--- Practicas/LABXX/Mines.h
+++ Practicas/LABXX/Mines.h
@@ -1,8 +1,9 @@
 #define RENGLON 15
 #define COLUMNA 15
 
+typedef unsigned char uint8; 
 
-void imprimir_tablero(int tablero[RENGLON][COLUMNA], int perder);
-void colocar_bombas(int tablero[RENGLON][COLUMNA], int bum);
-void revisar_alrededor(int Tablero[RENGLON][COLUMNA],int Fila, int Columna);
+void colocar_bombas(uint8 au8buscaminas[RENGLON][COLUMNA], uint8 u8bum);
+void imprimir_tablero(uint8 au8tablero[RENGLON][COLUMNA], uint8 u8perder);
+void revisar_alrededor(uint8 au8Tablero[RENGLON][COLUMNA],uint8 u8Fila, uint8 u8Columna);
 
